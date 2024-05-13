9708c66c42ff91bf20ff7adc536d488ef912815c
LukeDiBert <159164688+LukeDiBert@users.noreply.github.com>
2024-05-13 20:26:07 +0000

    animation

--- js/splashScene.js
+++ js/splashScene.js
@@ -7,34 +7,35 @@
 
 // this creates a class for the splash scene and makes a unique key that can be used later on
 class SplashScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super({ key: 'splashScene' })
   }
 
-  init (data) {
+  init(data) {
     // this sets the background color of the splash scene
     this.cameras.main.setBackgroundColor('#ffffff')
   }
   // this function puts Splash Scene into the console and loads the splash scene image into a key to be used later
-  preload () {
+  preload() {
     console.log('Splash Scene')
-    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
+    this.load.video('splashSceneBackground', './assets/IMHSplashScene.mp4')
   }
 
-  create (data) {
+  create(data) {
     // this is for my splash scene picture and to make it centered in the middle
-    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage = this.add.video(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.play(false);
     // screen dimensions for x 
     this.splashSceneBackgroundImage.x = 1920 / 2
     // screen dimensions on y 
     this.splashSceneBackgroundImage.y = 1080 / 2
   }
   // when the game updates every few hundred miliseconds it runs this function
-  update (time, delta){
-    if (time > 3000){
+  update(time, delta) {
+    if (time > 4850) {
       this.scene.switch('titleScene')
     }
   }
-}  
+}
 
 export default SplashScene
